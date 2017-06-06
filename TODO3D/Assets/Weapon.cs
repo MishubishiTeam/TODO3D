d04ad6de019a6d82d1817dacbf406404946e096c
Teo LAMBERT <lambeteo@iut-acy.local>
2017-06-06 16:56:46 +0200

    Classe weapon non débuguée + packages d'armes

---
+++ TODO3D/Assets/Weapon.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public string nom;
+    public float rearmTime;
+    public float damage;
+    public int maxCapacity;
+    public float reloadTime;
+    public float range;
+    public int actualCapacity;
+    bool canFire;
+    DateTime initRearming;
+    
+
+
+	// Use this for initialization
+	void Start () { 
+
+        canFire = true;
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (canFire == true)
+        {
+            if (Input.GetButtonDown("Fire1"))
+                tirer();
+
+            if (Input.GetKeyDown(KeyCode.R))
+                recharger();  // début du rechargement
+        }
+        else
+            recharger();
+
+
+        
+       
+		
+	}
+
+    void tirer()
+    {
+        if (actualCapacity > 0) // si le chargeur possède des balles
+        {
+            RaycastHit hit = new RaycastHit(); 
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)) //bool qui définit si qqchose a été touché (.position = pos actuelle, .transdirection = direction du tir, hit = objet RaycastHit)
+            {
+                float distance = hit.distance;
+                hit.transform.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+                initRearming = DateTime.Now;
+            }
+        }
+        else
+            recharger(); //début rechargement
+    }
+
+    void recharger()
+    {
+        canFire = false;
+        currentlyRearming();
+    }
+
+    bool currentlyRearming()
+    {
+        DateTime now = DateTime.Now;
+        if (now > initRearming.AddSeconds(reloadTime))
+        {
+            canFire = true;
+            return false;
+        }
+        else
+            return true;
+
+    }
+
+  
+}
