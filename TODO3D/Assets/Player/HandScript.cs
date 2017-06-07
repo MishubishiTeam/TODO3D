e01798974f2b8dba48219c023618caf57fd6b140
Hugo VENANCIO <venancih@iut-acy.local>
2017-06-07 18:44:06 +0200

    Mise à jour des prefabs du joueur et des armes

---
+++ TODO3D/Assets/Player/HandScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScript : MonoBehaviour
+{
+
+    public enum WeaponTypes
+    {
+        AK47,
+        Uzi,
+        M4A1
+    }
+
+    public WeaponTypes WeaponType;
+    public GameObject AkPrefab;
+    public GameObject UziPrefab;
+    public GameObject M4A1Prefab;
+
+    private GameObject weapon;
+    private Transform weaponTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        switch (WeaponType)
+        {
+            case WeaponTypes.AK47:
+                weapon = (GameObject)Instantiate(AkPrefab);
+                weaponTransform = weapon.transform;
+                weaponTransform.SetParent(transform.parent, false);
+                break;
+            case WeaponTypes.Uzi:
+                break;
+            case WeaponTypes.M4A1:
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
