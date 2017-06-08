ae2744a242a0c709adcab04faf1ec54b1828df4c
FennecStudent <louis.boulanger@etu.univ-smb.fr>
2017-06-08 08:22:00 +0200

    Mise à jour du script de caméra du joueur

--- TODO3D/Assets/Player/PlayerScript.cs
+++ TODO3D/Assets/Player/PlayerScript.cs
@@ -8,6 +8,7 @@ public class PlayerScript : MonoBehaviour
     private Animator anim;
     private Rigidbody player;
     private float currentRotation = 0F;
+    private float currentVerticalRotation = 0F;
     private Transform myTransform;
     private Transform cameraHolder;
 
@@ -38,18 +39,23 @@ public class PlayerScript : MonoBehaviour
         var inputX = Input.GetAxis("Horizontal");
         var inputY = Input.GetAxis("Vertical");
         var inputR = Mathf.Clamp(Input.GetAxis("Mouse X"), -1.0F, 1.0F);
+        var inputW = Mathf.Clamp(Input.GetAxis("Mouse Y"), -1.0F, 1.0F);
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             currentRotation += 180.0F;
         }
 
-        Vector3 moveVectorX = myTransform.forward * inputY;
-        Vector3 moveVectorY = myTransform.forward * inputX;
+        Vector3 moveVectorX = myTransform.forward * inputX;
+        Vector3 moveVectorY = myTransform.forward * inputY;
         Vector3 moveVector = (moveVectorX + moveVectorY).normalized * speed * Time.deltaTime;
 
         currentRotation = ClampAngle(currentRotation + (inputR * rotationSpeed));
+        currentVerticalRotation = Mathf.Clamp(ClampAngle(currentVerticalRotation + (inputW * rotationSpeed)), -30, 30);
+
         Quaternion rotationAngle = Quaternion.Euler(0.0F, currentRotation, 0.0F);
+
+        cameraHolder.GetChild(0).transform.rotation = Quaternion.Euler(-currentVerticalRotation, currentRotation, 0.0F);
 
         myTransform.position = myTransform.position + moveVector;
         myTransform.rotation = rotationAngle;
