779ede13a7a859d478fdc924fb67e395c11cce70
Hugo VENANCIO <venancih@iut-acy.local>
2017-06-09 13:42:00 +0200

    Ajout ui et minimap

---
+++ TODO3D/Assets/scripts/MiniMapScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour {
+
+
+    private GameObject Map;
+    public GameObject MiniMap;
+    public GameObject Joueur;
+    public GameObject PtRouge;
+    // Use this for initialization
+    void Start()
+    {
+        Map = GameObject.FindGameObjectWithTag("GameMap");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PtRouge.transform.position = new Vector3(
+            (-(float)Joueur.transform.position.x / (float)Map.GetComponent<Collider>().bounds.size.x) * 100+MiniMap.transform.position.x,
+            (-(float)Joueur.transform.position.z / (float)Map.GetComponent<Collider>().bounds.size.z) * 100 + MiniMap.transform.position.y,
+            0);
+    }
+}
