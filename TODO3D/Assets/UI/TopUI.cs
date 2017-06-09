4554b4c3d730fd7ede0ba1f1988fc4735ce3e2bf
Hugo VENANCIO <venancih@iut-acy.local>
2017-06-09 13:43:43 +0200

    Ajout UI et images

---
+++ TODO3D/Assets/UI/TopUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopUI : MonoBehaviour {
+
+    public GameObject Element;
+
+	// Use this for initialization
+	void Start () {
+        Element.transform.Translate(new Vector3(0,Screen.height/10+30, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
