da8fc326cc4ec1212e3bb147aea872a4de7b1cbb
Louis Boulanger <louis.boulanger@etu.univ-smb.fr>
2017-06-11 23:36:25 +0200

    Ajout texte munitions et fix bugs

--- TODO3D/Assets/Player/BulletScript.cs
+++ TODO3D/Assets/Player/BulletScript.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
 
+    [HideInInspector]
+    public float dmg;
+    [HideInInspector]
+    public PlayerScript2 sender;
+
     // Use this for initialization
     private void OnCollisionEnter(Collision collision)
     {
         var hit = collision.gameObject;
         var p = hit.GetComponent<PlayerScript2>();
-        if (p != null)
+        if (p != null && p != sender)
         {
+            p.ApplyDamage(dmg);
         }
 
         Destroy(gameObject);
