a5eebe43415efa5f0a5e4705f0e142bee3fd0c2f
Sebastien DUCASSE <ducassse@iut-acy.local>
2017-06-12 12:50:28 +0200

    ajout scoreboard

---
+++ TODO3D/Assets/scripts/ScoreBaord.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBaord : MonoBehaviour {
+    private bool pressed = true;
+    public GameObject scoreBoard;
+    
+	// Use this for initialization
+	void Start () {
+        //GameObject scoreBoard = GameObject.FindGameObjectWithTag("ScoreBoard");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+
+            scoreBoard.SetActive(true);
+        }
+        if(Input.GetKeyUp(KeyCode.Tab))
+        {
+            scoreBoard.SetActive(false);
+        }
+
+        
+            
+		
+	}
+}
